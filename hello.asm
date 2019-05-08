9a742eb32cf17893866a34d48503583a42b7c958
Le Quoc Viet <vietlq85@gmail.com>
2019-05-08 17:34:45 +0100

    initial commit

---
+++ hello.asm
@@ -0,0 +1,17 @@
+; $ nasm -felf64 hello.asm && ld hello.o && a.out
+
+	global _start
+
+	section .text
+
+_start: mov rax, 1       ; system call for write
+	mov rdi, 1       ; file handle 1 is STDOUT
+	mov rsi, message ; address of the string to output
+	mov rdx, 13      ; number of bytes
+	syscall          ; invoke operating system to do the write
+	mov rax, 60      ; system call for exit
+	xor rdi, rdi     ; exit code 0
+	syscall          ; invole operating system to exit
+
+	section .data
+message: db "Hello, World", 10 ; note the newline at the end
